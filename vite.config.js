7ebe91dbb8d945969891ba2f474221385dc5e388
Saivineeth <psv12082001@outlook.com>
2025-04-20 14:30:34 +0530

    Update vite.config.js for render site -2

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'vite';
+
 export default defineConfig({
   build: {
-    outDir: 'dist'  // Ensure it's set correctly
-  },
-  base: '/saivineeth-portfolio/' // Add this if your site is hosted on a subpath
+    outDir: 'dist'
+  }
 });
