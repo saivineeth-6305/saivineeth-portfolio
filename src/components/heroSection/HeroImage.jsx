6e1d48d5d96edae36ae91605f135337adcb57374
Saivineeth <psv12082001@outlook.com>
2025-04-20 19:12:14 +0530

    Update HeroImage.jsx

--- src/components/heroSection/HeroImage.jsx
+++ src/components/heroSection/HeroImage.jsx
@@ -4,13 +4,12 @@ const HeroImage = () => {
   return (
     <div className="relative self-end h-full w-full items-center justify-center">
       <div className=" h-full w-full">
-        <img
-          <img src="/images/hexapic2.png" alt="Abdelrahman Qassem" />
+       <img 
+  src="/images/hexapic2.png" 
+  alt="Abdelrahman Qassem"
+  className="w-auto h-auto md:max-w-[570px] sm:max-w-[380px] absolute bottom-[0px] z-10 left-[50%] -translate-x-[50%]"
+/>
 
-
-          alt="Abdelrahman Qassem"
-          className="w-auto h-auto md:max-w-[570px] sm:max-w-[380px] absolute bottom-[0px] z-10 left-[50%] -translate-x-[50%]"
-        />
         <div className="w-full h-full absolute  bottom-[-20%] -z-10 flex justify-center items-center rotate-90">
           <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-orange opacity-70  animate-[spin_20s_linear_infinite] " />
         </div>
