b304313030e41650407bf5d095bb96157f0366d8
Saivineeth <psv12082001@outlook.com>
2025-04-22 15:28:08 +0530

    Update vite.config.js - 22 / 3:28

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "", // Ensures correct asset paths
+  base: "./", // Adjust this based on deployment structure
 });
