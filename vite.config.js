2b1ce7978fa5c903f3358921ad054513acf915e7
Saivineeth <psv12082001@outlook.com>
2025-04-22 15:10:57 +0530

    Update vite.config.js - 22 / 3:10

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/saivineeth-portfolio/", // Ensure this matches your Render deployment path
-  build: {
-    outDir: "dist", // Ensure build outputs to the correct folder
-  },
-  server: {
-    port: 3000, // Optional: Set local dev server port
-  }
+  base: "", // Ensures correct asset paths
 });
