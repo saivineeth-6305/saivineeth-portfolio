d73f85fcef6aa54380778f2c3247417b4db4b5db
Saivineeth <psv12082001@outlook.com>
2025-04-22 15:24:04 +0530

    Update vite.config.js - 22 / 3:24

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/saivineeth-portfolio/", // Use correct deployment path
-  build: {
-    assetsDir: "assets", // Ensures images are correctly placed in the build
-  },
+  base: "", // Ensures correct asset paths
 });
