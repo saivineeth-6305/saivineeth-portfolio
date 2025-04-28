83a5ee053c67c8bc9f57290ca675d9e84fb90910
Your Full Name <your_email@example.com>
2025-04-28 12:01:38 +0530

    Updated vite.config.js to include base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  build: {
-    outDir: 'dist'
-  }
-=======
-  base: '/saivineeth-portfolio/', // Replace with your exact repository name
->>>>>>> 20a16745bd126aab09a725993560aab806cb3272
+build: {
+  outDir: 'dist'
+},
+base: '/saivineeth-portfolio/', // Replace with your exact repository name
 });
